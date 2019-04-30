b20670fafa7b7d114c459fbc563256d5226e70de
johnedquinn <johnedquinn@gmail.com>
2019-04-30 17:36:46 +0000

    Update CSS, JS, and JSON

--- www/assets/js/projects.js
+++ www/assets/js/projects.js
@@ -15,20 +15,21 @@ function get_projects(url) {
     get_json(url, function(data) {
 
         var html = [''];
-        html.push('<div>');
+        html.push('<div class="bg-light">');
 
         for (var i = 0; i < data.projects.length; i++) {
             if (i % 3 == 0) {
                 html.push('</div>');
-                html.push('<div class="row py-3"></div>');
-                html.push('<div class="card-deck">');
+                html.push('<div class="row py-4 bg-light"></div>');
+                html.push('<div class="card-deck bg-light">');
             }
             
             html.push(`<div class="card text-center">`);
-            html.push(`<img class="card-img-top" style="max-height: 5rem; max-width: 5rem;" src="${data.projects[i].imageURL}" alt="${data.projects[i].type}">`);
             html.push('<div class="card-body">');
-            html.push(`<h5 class="card-title text-dark"><a class="text-dark" href="${data.projects[i].link}">${data.projects[i].name}</a></h5>`);
+            html.push(`<h5 class="card-title text-dark"><a class="text-dark" rel="noopener noreferrer" target="_blank" href="${data.projects[i].link}">${data.projects[i].name}</a></h5>`);
             html.push(`<p class="card-text">${data.projects[i].description}</p>`);
+            html.push('</div><div class="card-footer">');
+            html.push(`<img style="max-height: 5rem; max-width: 1.5rem;" src="${data.projects[i].imageURL}" alt="${data.projects[i].type}">`);
             html.push(`<p class="card-text"><small class="text-muted">${data.projects[i].type}</small></p>`);
             html.push('</div>');
             html.push('</div>');
